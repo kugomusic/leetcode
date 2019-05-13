ac4f07566d41bc8a87fcf75c2d3c45bd04b07687
kugomusic <343192497@qq.com>
2019-05-14 00:04:44 +0800

    2019-05-14

--- src/firstPass/easy/_112/Solution.java
+++ src/firstPass/easy/_112/Solution.java
@@ -1,4 +1,28 @@
 package firstPass.easy._112;
 
+import firstPass.structure.TreeNode;
+
+/*
+简单的递归应用
+Simple use of recursion
+
+执行用时 : 1 ms, 在Path Sum的Java提交中击败了95.93% 的用户
+内存消耗 : 37.3 MB, 在Path Sum的Java提交中击败了66.96% 的用户
+ */
+
 public class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null) {
+            return root.val == sum;
+        }
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.hasPathSum(TreeNode.createTestData("[5,4,8,11,null,13,4,7,2,null,null,null,1]"), 22));
+    }
 }
